21c01eddbca84ae17596cafafd65c92c64a291a2
Jainam Jain <jainamjain2002@gmail.com>
2025-01-01 21:34:10 +0530

    Add Comments

--- modulo.c
+++ modulo.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 long long base,exponent,modulo;
 
+//This approach is based on the basic property of modulo that (a^b)mod(c) -> ((a mod (c))^b)mod(c)
+//partitioning the power into half at each iteration and taking mod each case in both even and odd powers
+
 
 long long evaluateExpression()
 {
     long long result = 1;
-    base = base%modulo;
+    base = base%modulo;  // Take modulo initially to handle cases where base >= modulo
     while(exponent>0)
-    {
+    {                                       
         if(exponent%2)
         {
-            result = (result*base)%modulo;    
+            result = (result*base)%modulo;    // If exponent is odd, multiply base with result and take modulo 
         }
-        exponent /=2;
+        exponent /=2;  //makes it a O(logn) solution
         base = (base*base)%modulo;
     }
     return result;
